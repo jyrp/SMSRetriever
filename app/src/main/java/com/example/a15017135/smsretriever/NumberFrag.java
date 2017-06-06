696dfcf7139f8f5499a7ac19f8cc26bf462382b6
15017135 <15017135@myrp.edu.sg>
2017-06-06 12:32:08 +0800

    layout done

---
+++ app/src/main/java/com/example/a15017135/smsretriever/NumberFrag.java
@@ -0,0 +1,29 @@
+package com.example.a15017135.smsretriever;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class NumberFrag extends Fragment {
+
+
+    public NumberFrag() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_number, container, false);
+    }
+
+}
